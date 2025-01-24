2bb209fa118a48ba45904f460911557c93088328
Загир Валитов <zagir-valitov@outlook.com>
2025-01-24 23:19:37 +0500

    Добавьте файлы проекта.

---
+++ HomeWork_Professional_03_L08_1.3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Work 1\n");
+
+string dirName = @"TextFiles";
+var stopWatch = new Stopwatch();
+
+if (Directory.Exists(dirName))
+{
+    string[] files = Directory.GetFiles(dirName, "*.txt");
+    if (files.Length >= 3)
+    {
+        Console.WriteLine("Sequential file reading...");
+        stopWatch.Start();
+
+        var count1 = GetNumberOfSpacesInTxtFile(files[0]);
+        var count2 = GetNumberOfSpacesInTxtFile(files[1]);
+        var count3 = GetNumberOfSpacesInTxtFile(files[2]);
+
+        stopWatch.Stop();
+        Console.WriteLine($"Time: {stopWatch.Elapsed.TotalMilliseconds} ms\n");
+
+        Console.WriteLine("Parallel file reading...");
+        stopWatch.Restart();
+
+        Task<int>[] tasks =
+        [
+            Task.Run(() => GetNumberOfSpacesInTxtFile(files[0])),
+            Task.Run(() => GetNumberOfSpacesInTxtFile(files[1])),
+            Task.Run(() => GetNumberOfSpacesInTxtFile(files[2]))
+        ];
+        Task.WaitAll(tasks);
+
+        stopWatch.Stop();
+        Console.WriteLine($"Time: {stopWatch.Elapsed.TotalMilliseconds} ms\n");
+    }
+}
+
+int GetNumberOfSpacesInTxtFile(string path)
+{
+    int count = 0;
+    string text;
+
+    using (var reader = new StreamReader(path))
+    {
+        text = reader.ReadToEnd();
+    }
+
+    foreach (var ch in text)
+        if (ch == ' ') count++;
+
+    return count;
+}
+
+
---
+++ HomeWork_Professional_03_L08_2.3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Work 2\n");
+
+string dirName = @"TextFiles";
+InfoNumberOfSpacesInTextFiles(dirName);
+
+void InfoNumberOfSpacesInTextFiles(string path)
+{
+    if (Directory.Exists(path))
+    {
+        Task[]tasksArray = new Task[1];
+        var stopWatch = new Stopwatch();
+        string[] files = Directory.GetFiles(path, "*.txt");
+        Array.Resize(ref tasksArray, files.Length);
+
+        Console.WriteLine("Sequential file reading...");
+        stopWatch.Start();
+
+        foreach (var file in files)
+            GetNumberOfSpacesInTextFile(file);
+
+        stopWatch.Stop();
+        Console.WriteLine("-----------------------------");
+        Console.WriteLine($"Time: {stopWatch.Elapsed.TotalMilliseconds} ms\n");
+
+
+        Console.WriteLine("Parallel file reading...");
+        stopWatch.Restart();
+
+        for (int i = 0; i < tasksArray.Length; i++)
+        {
+            int j = i;
+            tasksArray[j] = Task.Run(() => GetNumberOfSpacesInTextFile(files[j]));
+        }               
+        Task.WaitAll(tasksArray);
+        stopWatch.Stop();
+        Console.WriteLine("-----------------------------");
+        Console.WriteLine($"Time: {stopWatch.Elapsed.TotalMilliseconds} ms\n");
+    }
+}
+
+void GetNumberOfSpacesInTextFile(string path)
+{
+    int count = 0;
+    string text;
+
+    using (var reader = new StreamReader(path))
+    {
+        text = reader.ReadToEnd();
+    }
+    foreach (var ch in text)
+        if (ch == ' ') count++;
+    Console.WriteLine($"{path}\t - {count} spaces");    
+}
